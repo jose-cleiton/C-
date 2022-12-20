fc653a007398a2ac158789d9cd9f8136116d7ea2
jose-cleiton <cleitons835@gmail.com>
2022-12-20 14:43:02 -0300

    feat: :sparkles: Repetição for

---
+++ repeticao/for/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System;
+using System.Globalization;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Quantos números");
+
+         int x = int.Parse(Console.ReadLine());
+
+      for (int i =1; i<=x; i++){
+        Console.WriteLine("Digite um número");
+        int n = int.Parse(Console.ReadLine());
+        if (n % 2 == 0){
+          Console.WriteLine("Par");
+        }
+        else{
+          Console.WriteLine("Impar");
+        
+
+
+
+    }
+}
+  
+  }
+}
+}

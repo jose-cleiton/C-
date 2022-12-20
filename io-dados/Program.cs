06f447d2b03ffbdd63965b07192a347aed1437a7
jose-cleiton <cleitons835@gmail.com>
2022-12-20 11:06:27 -0300

    fix: :sparkles: Comparações lógicas

---
+++ io-dados/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //=========== eXcript.com ===========
+            //======== NetCoders.com.br  ========
+
+            //CONECTIVOS LÓGICOS
+
+            int num = 0;
+            Console.WriteLine("Informe um número no intervalo de 20 à 30.");
+            num = Convert.ToInt32( Console.ReadLine() );
+
+            if(num > 20 && num < 30)
+            {
+                Console.WriteLine("O número está no intervalo.");
+            }
+            else
+            {
+                Console.WriteLine("Número incorreto!");
+            }
+        }
+    }
+}
--- io-dados/obj/Debug/net7.0/io-dados.AssemblyInfo.cs
+++ io-dados/obj/Debug/net7.0/io-dados.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("io-dados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("C#")]
-[assembly: System.Reflection.AssemblyTitleAttribute("C#")]
+[assembly: System.Reflection.AssemblyProductAttribute("io-dados")]
+[assembly: System.Reflection.AssemblyTitleAttribute("io-dados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.

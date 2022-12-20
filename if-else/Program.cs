fb4983f2e9c3862cd5b2ed9272c71de6a325b966
jose-cleiton <cleitons835@gmail.com>
2022-12-20 11:36:05 -0300

    fix: :sparkles: sem chaves

--- if-else/Program.cs
+++ if-else/Program.cs
@@ -8,16 +8,12 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("Qual a hora?");
             int hora = int.Parse(Console.ReadLine());
-            if(hora<12){
-                Console.WriteLine("Bom dia!");
-            }
-            else if(hora>=12 && hora<18){
+            if(hora<12) Console.WriteLine("Bom dia!");
+            
+            else if(hora<18) Console.WriteLine("Baa tarde!");
+        
+            else  Console.WriteLine("Boa noite!");
 
-                Console.WriteLine("Baa tarde!");
-        }
-            else if(hora>=18){
-                Console.WriteLine("Boa noite!");
-}
         }
     }
 }
